42c75180608f8c461a9a862a5c17eb929d5aa8b2
Paul Cockrell <paulcockrell@gmail.com>
2023-08-20 20:18:33 +0100

    Setup bracket lib
    Include bracket lib and setup basic game state and render window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Paul and Moss and Bob");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .build()?;
+
+    main_loop(context, State {})
 }
