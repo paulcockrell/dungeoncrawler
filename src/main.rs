b1bf502bf46a2cba6e85d02f15c420e328343f9e
Paul Cockrell <paulcockrell@gmail.com>
2023-08-21 12:43:53 +0100

    Add basic map and render it

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-use bracket_lib::prelude::*;
+mod map;
 
-struct State {}
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
+
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello, Paul and Moss and Bob");
+        self.map.render(ctx);
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
