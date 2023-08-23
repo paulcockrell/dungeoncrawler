37c696d5baf6e6780dc5c670e9d05b84011eaa88
Paul Cockrell <paulcockrell@gmail.com>
2023-08-23 17:50:18 +0100

    Draw entities to world
    Draw the entities, at the moment that is the player

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,3 +1,4 @@
+mod entity_render;
 mod map_render;
 mod player_input;
 
@@ -7,5 +8,6 @@ pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
         .build()
 }
