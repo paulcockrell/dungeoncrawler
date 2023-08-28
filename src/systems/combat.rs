1c23277a0d9031ad3eb9ecd52960633de93579e8
Paul Cockrell <paulcockrell@gmail.com>
2023-08-28 10:28:51 +0100

    Don't crash when player looses
    Only remove an entity if it is an enemy. Introduces new bug where player hp can go below 0

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttack)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut attackers = <(Entity, &WantsToAttack)>::query();
@@ -9,18 +10,22 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         .iter(ecs)
         .map(|(entity, attack)| (*entity, attack.victim))
         .collect();
-    for (message, victim) in &victims {
+
+    for (message, victim) in victims.iter() {
+        let is_player = ecs
+            .entry_ref(*victim)
+            .unwrap()
+            .get_component::<Player>()
+            .is_ok();
         if let Ok(health) = ecs
             .entry_mut(*victim)
             .unwrap()
             .get_component_mut::<Health>()
         {
-            println!("Health before attack: {}", health.current);
             health.current -= 1;
-            if health.current < 1 {
+            if health.current < 1 && !is_player {
                 commands.remove(*victim);
             }
-            println!("Health after attack: {}", health.current);
         }
         commands.remove(*message);
     }
