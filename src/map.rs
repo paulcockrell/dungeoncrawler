32122d1631a48ecf2dea07901381b75bbf52afcd
Paul Cockrell <paulcockrell@gmail.com>
2023-08-29 09:13:43 +0100

    Render visited dungeon regions
    Render visited dungeon regions in a 'tinted' color to show they have been visited. The players field of vision remains fully illuminated.

--- src/map.rs
+++ src/map.rs
@@ -7,18 +7,20 @@ pub enum TileType {
     Floor,
 }
 
-pub struct Map {
-    pub tiles: Vec<TileType>,
-}
-
 pub fn map_idx(x: i32, y: i32) -> usize {
     ((y * SCREEN_WIDTH) + x) as usize
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+    pub revealed_tiles: Vec<bool>,
 }
 
 impl Map {
     pub fn new() -> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
+            revealed_tiles: vec![false; NUM_TILES],
         }
     }
 
