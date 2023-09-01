9cf3f6f0080224229d68ad3672e6d2fb76349c82
Paul Cockrell <paulcockrell@gmail.com>
2023-09-01 15:58:15 +0100

    Remove skip turn auto heal
    You can now collect healing potions, so remove the skip turn to heal feature

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -85,16 +85,6 @@ pub fn player_input(
             }
         }
 
-        if !did_something {
-            if let Ok(health) = ecs
-                .entry_mut(player_entity)
-                .unwrap()
-                .get_component_mut::<Health>()
-            {
-                health.current = i32::min(health.max, health.current + 1);
-            }
-        }
-
         *turn_state = TurnState::PlayerTurn;
     }
 }
