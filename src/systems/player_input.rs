f6fbafe7873100fc9a3a976f7e888fda7cc8f27c
Paul Cockrell <paulcockrell@gmail.com>
2023-09-02 14:13:49 +0100

    Game completed
    Add release optimisations. Update template.ron entity lables.

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -67,7 +67,6 @@ pub fn player_input(
             .unwrap();
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
-        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -75,7 +74,6 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
-                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -86,7 +84,6 @@ pub fn player_input(
                 });
 
             if !hit_something {
-                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
